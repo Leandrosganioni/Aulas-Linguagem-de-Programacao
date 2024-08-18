a037b5358ac149775f34c409629e6cffdbf6cbca
Leandro Sganioni <leandrosgani@gmail.com>
2024-08-18 12:06:16 -0300

    fazendo exercicio2

--- exercicios_php/index.php
+++ exercicios_php/index.php
@@ -7,7 +7,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Hoje é dia <?php echo date("d/m/Y");?></h1>
+    <h1>Hoje é dia <?php 
+    
+    echo date("d/m/Y");
+    
+    
+    ?></h1>
     <form action="resposta.php" method="POST"> <!-- sempre tem que ter action e method -->
         <input type="text" name="valor" />
         <button type="submit">Enviar</button>
