632b19572dab858e7ce975a567a0d9ccbde0050a
Leandro Sganioni <leandrosgani@gmail.com>
2024-08-20 20:22:10 -0300

    Exercicio 1 de php adicionado

--- exercicios_php/resposta.php
+++ exercicios_php/resposta.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body> <!-- variável começa com $ -->
+<body> 
     <?php 
-        $valor = $_POST["valor"]; 
-        echo "Valor informado: $valor"; 
+    //este é um comentário de uma linha
+        $valor = (int)$_POST['valor']; 
+        $valor = (float) $_POST['valor'];
+        echo 'Valor informado: '.$valor; 
         
     ?>
-    <!-- sempre escrever com aspas duplas -->
 </body>
 </html>
