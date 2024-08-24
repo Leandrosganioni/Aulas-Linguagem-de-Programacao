076e7a5578c258fa62aace496f03e31aa3f4cec9
Leandro Sganioni <leandrosgani@gmail.com>
2024-08-24 18:02:44 -0300

    Exercício 11 finalizado

--- lista2/exer11.php
+++ lista2/exer11.php
@@ -3,13 +3,13 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 9</title>
+    <title>Exercício 11</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
 <body>
     <div class="container">
-        <h1>Exercício 9</h1>
-        <form action="exer9resp.php" method="POST">
+        <h1>Exercício 11</h1>
+        <form action="exer11resp.php" method="POST">
             <div class="row">
                 <div class="col">
                     <label for="raio" class="form-label" nome="raio">Digite o raio do círculo:</label>
--- lista2/exer11resp.php
+++ lista2/exer11resp.php
@@ -3,16 +3,16 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 9</title>
+    <title>Exercício 11</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Resposta do exercício 9</h1>
+    <h1>Resposta do exercício 11</h1>
     <?php
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
             try{
                 $raio = (int) $_POST['raio'] ?? 0;
-                $resultado = (3.14 * ($raio ** 2));
+                $resultado = (2 * 3.14 * $raio);
                 echo "<p>Área do retangulo: $resultado</p>";
             }
             catch(Exception $e) {
