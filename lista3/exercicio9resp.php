2c33756143d15b0d31d34bf3733074273aa02781
Leandro Sganioni <leandrosgani@gmail.com>
2024-09-01 23:11:03 -0300

    Adicionado exercicio 10

--- lista3/exercicio9resp.php
+++ lista3/exercicio9resp.php
@@ -3,24 +3,24 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 8</title>
+    <title>Exercício 9</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok7YctnYmDr5pNlyT2bRjXh0JMhjY7hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Resposta do exercício 8</h1>
+    <h1>Resposta do exercício 9</h1>
     <?php
         if($_SERVER['REQUEST_METHOD'] == 'POST')
         {
             try
             { 
                 $valor = (int) $_POST['valor'] ?? 0;
-                do {
-                    echo $valor . "\n";
-                    $valor--;
-                } while ($valor >= 0);
-                echo "\n";
-            
-            } 
+                $soma = 0;
+                $fatorial = 1;
+                for ($i = 1; $i <= $valor; $i++) {
+                    $fatorial *= $i;
+                }
+                echo "Fatorial: $fatorial";
+            }
             catch(Exception $e) 
             {
                 echo "Erro!".$e->getMessage();
