5a4653a1ae42b2ed2d6281aa6927742a8ef802a8
Leandro Sganioni <leandrosgani@gmail.com>
2024-09-08 14:18:06 -0300

    Exercício 1

--- lista4/exercicio1.php
+++ lista4/exercicio1.php
@@ -1,25 +1,24 @@
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 1 lista 4</title>
+    <title>Exercício 1</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
-<body>
-    <h3>Exercício 1</h3>
+  <body class="container">
+    <h1>Exercício 1</h1>
     <form action="exercicio1resp.php" method="POST">
         <div class="row">
             <div class="col">
-                <input type="text" name="valor" class="form-control">
+                <label for="palavra" class="form-label">Informe uma palavra: </label>
+                <input type="text" class="form-control" name="palavra" id="palavra">
             </div>
-            <div class="col">
-                <label for="num" class="form-label">Informe o número:</label>
-                <input type="number" class="form-control" name="num" id="num">
-            </div>
+        </div>
+        <div class="row">
             <div class="col">
                 <button type="submit" class="btn btn-primary">Enviar</button>
             </div>
         </div>
-    </form> 
-</body>
+    </form>
+  </body>
