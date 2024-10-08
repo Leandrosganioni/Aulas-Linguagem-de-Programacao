da69b0817c43c5081c7861b5272bb658e2074cd2
Leandro Sganioni <leandrosgani@gmail.com>
2024-10-08 19:13:51 -0300

    Projeto associação

--- lista2/exer1.php
+++ lista2/exer1.php
@@ -6,7 +6,12 @@
     <title>Exercício 1</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
-  <body class="container">
+  <body>
+    <?php
+        include("nav.php");
+        include_once("nav.php");
+    ?>
+    <main class="container">
     <h1>Exercício 1</h1>
     <form action="exer1resp.php" method="POST">
         <div class="row">
@@ -25,4 +30,5 @@
             </div>
         </div>
     </form>
+    </main>
   </body>
